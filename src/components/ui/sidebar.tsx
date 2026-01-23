67a9b3683ac888067dbda5d88ee44cbd0c194c64
Shuvendu Singha <shuvendusingha17@gmail.com>
2026-01-23 17:34:49 +0000

    Try fixing this error: `Runtime FirebaseError: Performance: Attribute va

--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -572,6 +572,7 @@ const SidebarMenuButton = React.forwardRef<
         data-size={size}
         data-active={isActive}
         className={cn(sidebarMenuButtonVariants({ variant, size }), className)}
+        data-firebase-performance-ignore="true"
         {...props}
       />
     )
