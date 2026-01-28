558faeb21b50e20a004bb615b9236e0210db7175
Shuvendu Singha <shuvendusingha17@gmail.com>
2026-01-28 08:42:17 +0000

    there is two cross button showing in blog details modal

--- src/app/blog/blog-post-dialog.tsx
+++ src/app/blog/blog-post-dialog.tsx
@@ -141,10 +141,6 @@ export default function BlogPostDialog({ post, isOpen, onClose }: BlogPostDialog
                     </div>
                 </article>
             </ScrollArea>
-             <DialogClose className="absolute right-4 top-4 rounded-sm opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-accent data-[state=open]:text-muted-foreground bg-black/50 text-white hover:bg-black/75">
-                <X className="h-5 w-5" />
-                <span className="sr-only">Close</span>
-            </DialogClose>
         </DialogContent>
     </Dialog>
   );
